7e3094b99cdbf3a811aafeec0471a6da3aa4873f
Daniel Leyva <leyvachavez1912@gmail.com>
2026-09-23 00:55:23 -0700

    Ahi te va we

--- agente.py
+++ agente.py
@@ -1,1 +1,12 @@
-print("hola")
+import random
+
+FILAS = 5
+COLUMNAS = 5
+AGENTE = 'A'
+elementos = ['.', 'P', 'X']
+pesos = [0.70, 0.15, 0.15]
+
+map = [[random.choices(elementos, weights=pesos)[0] for i in range(COLUMNAS)] for i in range(FILAS)]
+
+for fila in map:
+    print(fila)
